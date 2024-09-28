a866af448968fa49e2f88f60656713e82792099a
Joshua Marshall-law <marshalllawjoshua@gmail.com>
2024-09-28 20:15:14 +0100

    Add files via upload

---
+++ FInal NEA/Class1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FInal_NEA
+{
+    internal class graphofconvo
+    {
+        public node root;
+
+
+       
+        public bool Convo(bool ongoing, int count, word[] words)
+        {
+            bool netS;
+            int PScore = 1;
+            int Nscore = 1;
+            Naivebayes NB1= new Naivebayes(); // new naive bayes class for use of the functions involved
+            Request[] currentrequest = new Request[5]; //initialised array of the request class for use in program
+            node point; // "tracking" node so we knew where we are 
+            while (ongoing == true)
+            {
+                root = new node();
+                for (int i = 0; i < 5; i++)
+                {
+                    if (count > 2)
+                    {
+                        ongoing = false;
+                    }
+                    currentrequest[count] = new Request(); //instanciates request class
+                    //start node as count will = 0 at start 
+                    if (count == 0)
+                    {
+                        root.userprompt = "Hey, how was your day";
+                        
+                        root.startp();
+                        string startinput = Console.ReadLine().ToLower();
+                        currentrequest[count].title(startinput); // sets name of objecrt as userinput
+                        currentrequest[count].setnscore(NB1.negativescores(startinput, words)); // sets n score with result form function
+                        currentrequest[count].setpscore(NB1.positivescores(startinput, words)); // sets p score w result from function 
+                        root.determineSentiment(currentrequest[count].Pscore, currentrequest[count].Nscore);  // determines sentiment for the node 
+                        root.getuserstring(root.sentiment); //responds based on sentiment 
+                        if (root.sentiment == true)
+                        {
+                            PScore++;
+                        }
+                        else
+                        {
+                            Nscore++;
+                        }
+                    
+                    }
+                    else
+
+                    {
+                        node current = root;
+                        Console.ReadLine();
+                        root.nextquestion(count - 1);
+                        root.startp();
+                        string userinput = Console.ReadLine().ToLower();
+                        currentrequest[count].title(userinput);
+                        currentrequest[count].setnscore(NB1.negativescores(userinput, words)); //sets negative score for node
+                        currentrequest[count].setpscore(NB1.positivescores(userinput, words));//sets positive score for node
+                        root.determineSentiment(currentrequest[count].Pscore, currentrequest[count].Nscore);  // determines sentiment of root node 
+                        if (root.sentiment == true) // changes nodes based on sentiment. 
+                        {
+                            current.positive1 = new node();
+                            root.getuserstring(root.sentiment); // determines response given the sentiment
+                            point = current.positive1;
+                            root = point;
+                            point = null;
+                            PScore++;
+                            // completes transference of node to keep track of conversation   
+                        }
+                        else if (root.sentiment == false)
+                        {
+                            current.negative1 = new node();
+                            root.getuserstring(root.sentiment); // determines response based on sentiment 
+                            point = current.negative1;
+                            root = point;
+                            point = null;
+                            Nscore++;
+                          // completes transference of nodes to keep track of conversation
+                        }
+                        
+                    }
+                    //if moving to negative/positive pull node userprompt from random bank of user prompts from given fields (p/n)
+                    count++;
+                }
+            }
+
+            if (PScore > Nscore) // returns how overrall day went 
+            {
+                netS = true;
+            }
+            else
+            {
+                netS = false;
+            }
+
+            return netS;
+        }
+    }
+
+}
---
+++ FInal NEA/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FInal_NEA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           bool nets; 
+           Naivebayes NB= new Naivebayes(); // instanciates naive bayes class
+           word[] words = NB.generateProbabilities();  // generates probabilities of new data 
+           word[] load = NB.loadfromcsv(); // loads previous system data 
+           word[] words1 = NB.average(load, words); // averages matches and adds new data to word array 
+           // NB.write(words1); // writes new weights to a csv (still needs fixing)
+           graphofconvo start= new graphofconvo(); // instantiates graphofconvo class
+           nets = start.Convo(true, 0 , words1); // gets boolean of day 
+            if (nets == false) // selection 
+            {
+                Console.WriteLine("I'm sorry you didn't have a good day my friend");
+            }
+            else
+            {
+                Console.WriteLine("I'm so happy you had a good day my friend");
+            
+            }
+            Console.WriteLine("Thank you so much for talking with me today friend, I hope tomorrow is better then today!!");
+            Console.WriteLine("Have a blessed rest of your day.");
+            Console.ReadLine(); 
+        }
+
+
+
+    }
+}
+
+ 
+
+
